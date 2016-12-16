3a525884eed9d8371956b61b49dac30acc6777ce
Alex <koralexander@gmail.com>
2016-12-16 09:56:24 +0200

    Added update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('58539a1fee256e19e3ac88d7')
+  }, {
+    $set: {
+      name: 'Alexander'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  db.close();
+});
